7ac038316f085cc88cc9edd0d9ab74230be00157
tomwwang <tomwwang@tencent.com>
2021-09-26 14:32:40 +0800

    test: 测试

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -4,6 +4,8 @@ import NuxtLogo from '@/components/NuxtLogo.vue'
 describe('NuxtLogo', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(NuxtLogo)
+    console.log(1)
+    console.log(2)
     expect(wrapper.vm).toBeTruthy()
   })
 })
